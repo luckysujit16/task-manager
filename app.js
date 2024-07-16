8976f725c787aba52d1119452187277070c19307
luckysujit16 <167292136+luckysujit16@users.noreply.github.com>
2024-07-16 12:23:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-
+// app.js
 const express = require("express");
 const app = express();
 const port = 3000;
+const taskRoutes = require("./test/routes/tasks");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/", taskRoutes);
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log("Something bad happened", err);
+  }
+  console.log(`Server is listening on ${port}`);
+});
 
 module.exports = app;
