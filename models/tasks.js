8e0dd71271d3b41fe240874c14086f76daabd348
luckysujit16 <167292136+luckysujit16@users.noreply.github.com>
2024-07-17 10:52:02 +0530

    Update tasks.js
    Updated Priority status

--- models/tasks.js
+++ models/tasks.js
@@ -1,10 +1,10 @@
-// models/task.js
 class Task {
-  constructor(id, title, description, completed) {
+  constructor(id, title, description, completed, priority) {
     this.id = id;
     this.title = title;
     this.description = description;
     this.completed = completed;
+    this.priority = priority || 'medium'; // Default priority is 'medium'
   }
 }
 
